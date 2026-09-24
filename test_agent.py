a83d8f6d80dcd622168a8e4e2ea86481fa3194c8
Prakhar Agrawal <proton1250@gmail.com>
2026-09-23 22:31:24 -0400

    Update test_agent.py

--- test_agent.py
+++ test_agent.py
@@ -20,8 +20,7 @@ def score_model(dataset):
     return response.json()
 
 if __name__ == '__main__':
-    # 1. Set your new token here temporarily for testing, or export it in your terminal
-    os.environ["DATABRICKS_TOKEN"] = "dapiaf52f74f0b0beefba37f1c245d580bd5"
+    # 1. Set token here temporarily for testing, or export in your terminal
 
     # 2. Create the test payload
     test_data = pd.DataFrame({
